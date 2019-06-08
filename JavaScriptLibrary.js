cc2f495ee0a0e64dc47eaa9e2d03ee9053a1a3a7
Jeffrey Layton <c3321126@uon.edu.au>
2019-06-08 17:58:01 +1000

    js

--- JavaScriptLibrary.js
+++ JavaScriptLibrary.js
@@ -1,8 +1,25 @@
 
-//Function assigns the submitted ID to the form and submits it (gamePage.jsp). 
-//Used for clicking the images
-function clickHandler(id){
-	var h = document.getElementById("hiddenForm");
-	h.value = id;
-	document.forms["hiddenF"].submit();
+function clickHandlerCatBar(value){
+	var h = document.getElementById("categoryBar");
+	h.categoryBar = value;
+	document.forms["categoryBar"].submit();
 }
+
+function clickHandlerNavBar(value){
+	var h = document.getElementById("navagationBar");
+	h.navagationBar = value;
+	document.forms["navagationBar"].submit();
+}
+
+function clickHandlerViewTicket(value){
+	var h = document.getElementById("viewTicket");
+	h.viewTicket = value;
+	document.forms["viewTicket"].submit();
+}
+
+function clickHandlerViewArticle(value){
+	var h = document.getElementById("viewArticle");
+	h.viewArticle = value;
+	document.forms["viewArticle"].submit();
+}
+
